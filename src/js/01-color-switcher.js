f10acf56e2f93b098c7fd33cc0343992ba73fdcc
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2023-02-03 15:27:39 +0100

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+    startButtonEl: document.querySelector('button[data-start]'),
+    stopButtonEl: document.querySelector('button[data-stop]'),
+    bodyEl: document.querySelector('body'),
+};
 
+refs.startButtonEl.addEventListener('click', onStartClick);
+refs.stopButtonEl.addEventListener('click', onStopClick);
+
+
+function onStartClick(evt) {
+    if (refs.stopButtonEl.disabled) {
+        refs.stopButtonEl.disabled = false;
+    }
+    refs.startButtonEl.disabled = true;
+    return interval = setInterval(() => {
+        addRandomBackgroundColor(refs.bodyEl);
+    }, 1000);
+}
+function onStopClick() {
+    if (refs.startButtonEl.disabled) {
+        refs.startButtonEl.disabled = false;
+    }
+    refs.stopButtonEl.disabled = true;
+    clearInterval(interval);
+}
+function addRandomBackgroundColor(el) {
+    el.style.backgroundColor = getRandomHexColor();
+}
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
