d0cff73816a7ee0ef8784573c7baec011f07513a
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2023-02-05 00:14:32 +0100

    bag fix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,7 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
+
 const refs = {
     timePickerEl: document.querySelector('input#datetime-picker'),
     startBtEl: document.querySelector('button[data-start]'),
